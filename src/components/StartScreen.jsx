db2234e0d31fe5e6d8ccc5ac27ec99f051957cca
Ahmadsafikhan <safionair@gmail.com>
2023-08-14 23:32:39 +0500

    changes

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const StartScreen = ({ numQuestions, dispatch }) => {
   return (
     <div className="flex flex-col items-center justify-center h-full">
-      <h2 className="text-3xl font-semibold mb-4">Welcome to React Quiz!</h2>
+      <h2 className="text-3xl font-semibold mb-4">Welcome to Quiz!</h2>
       <h3 className="text-lg text-gray-600 mb-6">{numQuestions} questions to test your Pakistan History Knowledge</h3>
       <button
         onClick={() => dispatch({ type: 'start' })}
